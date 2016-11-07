e16866d361c85a4ae6df940ce62abce3afe32f22
Richard Smith <richard-llvm@metafoo.co.uk>
2016-11-07 16:47:20 +0000

    Add -O0 support for @llvm.invariant.group.barrier by discarding it if it gets to ISel.
    Differential Revision: https://reviews.llvm.org/D26292
    
    git-svn-id: https://llvm.org/svn/llvm-project/llvm/trunk@286119 91177308-0d34-0410-b5e6-96231b3b80d8

--- lib/CodeGen/SelectionDAG/SelectionDAGBuilder.cpp
+++ lib/CodeGen/SelectionDAG/SelectionDAGBuilder.cpp
@@ -5410,6 +5410,7 @@ SelectionDAGBuilder::visitIntrinsicCall(const CallInst &I, unsigned Intrinsic) {
   }
   case Intrinsic::annotation:
   case Intrinsic::ptr_annotation:
+  case Intrinsic::invariant_group_barrier:
     // Drop the intrinsic, but forward the value
     setValue(&I, getValue(I.getOperand(0)));
     return nullptr;
